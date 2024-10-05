18fd42a2286f475e2aa7f964c910501fb33db65b
rishabhpandey106 <rishabhpandey230@gmail.com>
2024-10-05 15:15:32 +0530

    added car price predictor

---
+++ Car Price Predictor/car-predictor/tailwind.config.ts
@@ -0,0 +1,33 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      "animation": {
+            shimmer: "shimmer 2s linear infinite"
+          },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      "keyframes": {
+            shimmer: {
+              from: {
+                "backgroundPosition": "0 0"
+              },
+              to: {
+                "backgroundPosition": "-200% 0"
+              }
+            }
+          }
+    },
+  },
+  plugins: [],
+};
+export default config;
